1dab1bf8bb9316e82faf1c336433d90b5e84c7f2
Kayla Fang <kfang00@stuy.edu>
2018-11-09 04:45:40 -0500

    did addWordDiagonal

--- WordSearch.java
+++ WordSearch.java
@@ -100,4 +100,31 @@ public class WordSearch{
 	}
 	return true;
     }
+
+    /**Attempts to add a given word to the specified position of the WordGrid.
+     *The word is added from top left to bottom right, must fit on the WordGrid,
+     *and must have a corresponding letter to match any letters that it overlaps.
+     *
+     *@param word is any text to be added to the word grid.
+     *@param row is the vertical locaiton of where you want the word to start.
+     *@param col is the horizontal location of where you want the word to start.
+     *@return true when the word is added successfully. When the word doesn't fit,
+     *or there are overlapping letters that do not match, then false is returned.
+     */
+    public boolean addWordDiagonal(String word,int row, int col){
+        char[][] previous = before(data);
+	if ((row + word.length()) > data.length) {
+	    return false;
+	}
+        for (int a = 0; a < word.length(); a++) {
+	    if ((data[row + a][col + a] == '_') || (data[row + a][col + a] == word.charAt(a))) { 
+	        data[row + a][col + a] = word.charAt(a);
+	    }
+	    else {
+		data = previous;
+		return false;
+	    }
+	}
+	return true;
+    }
 }
